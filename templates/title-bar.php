c4bdeb837536c52c3b1df4899b8ab168c76b3d7c
David Rothfarb <dave_rothfarb@dfci.harvard.edu>
2022-02-02 17:28:38 -0500

    More work on factfinder pages, starting on generic inside pages

--- templates/title-bar.php
+++ templates/title-bar.php
@@ -23,8 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<?php if ( $title ) : ?>
 						<?php // Add entry-title for rich snippets. ?>
 						<?php $entry_title_class = ( Avada()->settings->get( 'disable_date_rich_snippet_pages' ) && Avada()->settings->get( 'disable_rich_snippet_title' ) ) ? 'entry-title' : ''; ?>
-						<h1 class="<?php echo esc_attr( $entry_title_class ); ?>"><?php echo $title; // phpcs:ignore WordPress.Security.EscapeOutput ?></h1>
-
+            <div class="content-margin">
+              <h1 class="<?php echo esc_attr( $entry_title_class ); ?>"><?php echo $title; // phpcs:ignore WordPress.Security.EscapeOutput ?></h1>
+            </div>
 						<?php if ( $subtitle ) : ?>
 							<h3><?php echo $subtitle; // phpcs:ignore WordPress.Security.EscapeOutput ?></h3>
 						<?php endif; ?>
